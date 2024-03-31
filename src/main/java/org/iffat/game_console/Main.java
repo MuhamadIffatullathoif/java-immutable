31c0cf4a3690af455c03c0d8103f902f17d07851
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-31 21:53:07 +0700

    22. Final Challenge (Pirates Continued, Part 2)

--- src/main/java/org/iffat/game_console/Main.java
+++ src/main/java/org/iffat/game_console/Main.java
@@ -1,9 +1,9 @@
 package org.iffat.game_console;
 
 import org.iffat.game_console.game.GameConsole;
-import org.iffat.game_console.game.ShooterGame;
 import org.iffat.game_console.pirate.Pirate;
 import org.iffat.game_console.pirate.PirateGame;
+import org.iffat.game_console.pirate.Town;
 import org.iffat.game_console.pirate.Weapon;
 
 public class Main {
@@ -14,7 +14,7 @@ public class Main {
 //        console.playGame(playerIndex);
         Weapon weapon = Weapon.getWeaponByChar('P');
         System.out.println("weapon = " + weapon + ", hitPoints="
-                + weapon.getHitPoints() + ", minLevel=" +weapon.getMinLevel());
+                + weapon.getHitPoints() + ", minLevel=" + weapon.getMinLevel());
 
         var list = Weapon.getWeaponsByLevel(1);
         list.forEach(System.out::println);
@@ -22,12 +22,16 @@ public class Main {
         Pirate tim = new Pirate("Tim");
         System.out.println(tim);
 
-        PirateGame.getTowns(0).forEach(System.out::println);
+        PirateGame.getTowns(0).forEach(town -> System.out.println(town.information()));
         System.out.println("-".repeat(90));
-        PirateGame.getTowns(1).forEach(System.out::println);
+        PirateGame.getTowns(1).forEach(town -> System.out.println(town.information()));
 
         var console = new GameConsole<>(new PirateGame("The Pirate Game"));
         int playerIndex = console.addPlayer();
         console.playGame(playerIndex);
+
+        Town bridgeTown = new Town("Bridgetown", "Barbados", 0);
+        System.out.println(bridgeTown);
+        System.out.println(bridgeTown.information());
     }
 }
--- src/main/java/org/iffat/game_console/pirate/Pirate.java
+++ src/main/java/org/iffat/game_console/pirate/Pirate.java
@@ -1,13 +1,10 @@
 package org.iffat.game_console.pirate;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public final class Pirate extends Combatant {
 
-    private final List<String> townsVisited = new LinkedList<>();
+    private final List<Town> townsVisited = new LinkedList<Town>();
     private List<Loot> loot;
     private List<Combatant> opponents;
     private List<Feature> features;
@@ -19,36 +16,85 @@ public final class Pirate extends Combatant {
 
     boolean useWeapon() {
 
-        System.out.println("Used the " + super.getCurrentWeapon());
-        return visitNextTown();
+        int count = opponents.size();
+        if (count > 0) {
+            int opponentIndex = count - 1;
+            if (count > 1) {
+                opponentIndex = new Random().nextInt(count + 1);
+            }
+            Combatant combatant = opponents.get(opponentIndex);
+            if (super.useWeapon(combatant)) {
+                opponents.remove(opponentIndex);
+            } else {
+                return combatant.useWeapon(this);
+            }
+        }
+        return false;
     }
 
     boolean visitTown() {
 
-        List<String> levelTowns = PirateGame.getTowns(value("level"));
+        List<Town> levelTowns = PirateGame.getTowns(value("level"));
         if (levelTowns == null) return true;
-        String town = levelTowns.get(value("townIndex"));
+        Town town = levelTowns.get(value("townIndex"));
         if (town != null) {
             townsVisited.add(town);
+            loot = town.loot();
+            opponents = town.opponents();
+            features = town.features();
             return false;
         }
         return true;
     }
 
+    boolean hasExperiences() {
+        return (features != null && features.size() > 0);
+    }
+
+    boolean hasOpponents() {
+        return (opponents != null && opponents.size() > 0);
+    }
+
     public String information() {
 
-        var current = ((LinkedList<String>) townsVisited).getLast();
+        var current = ((LinkedList<Town>) townsVisited).getLast();
         String[] simpleNames = new String[townsVisited.size()];
-        Arrays.setAll(simpleNames, i -> townsVisited.get(i).split(",")[0]);
+        Arrays.setAll(simpleNames, i -> townsVisited.get(i).name());
         return "--> " + current +
                 "\n" + super.information() +
                 "\n\ttownVisited=" + Arrays.toString(simpleNames);
     }
 
+    boolean findLoot() {
+
+        if (loot.size() > 0) {
+            Loot item = loot.remove(0);
+            System.out.println("Found " + item + "!");
+            adjustValue("score", item.getWorth());
+            System.out.println(name() + "'s score is now " + value("score"));
+        }
+
+        if (loot.size() == 0) {
+            return visitNextTown();
+        }
+        return false;
+    }
+
+    boolean experienceFeature() {
+
+        if (features.size() > 0) {
+            Feature item = features.remove(0);
+            System.out.println("Ran into " + item + "!");
+            adjustHealth(item.getHealthPoints());
+            System.out.println(name() + "'s health is now " + value("health"));
+        }
+        return (value("health") <= 0);
+    }
+
     private boolean visitNextTown() {
 
         int townIndex = value("townIndex");
-        var towns = PirateGame.getTowns(value("level"));
+        List<Town> towns = PirateGame.getTowns(value("level"));
         if (towns == null) return true;
         if (townIndex >= (towns.size() - 1)) {
             System.out.println("Leveling up! Bonus: 500 points!");
