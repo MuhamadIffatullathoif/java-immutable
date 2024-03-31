cefa577e4d1ccc90b984a5a618f968fc29e7e76f
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-31 07:56:54 +0700

    10. Challenge: Use unmodifiable collections

--- src/main/java/org/iffat/immutable_challenge/BankAccount.java
+++ src/main/java/org/iffat/immutable_challenge/BankAccount.java
@@ -1,12 +1,16 @@
 package org.iffat.immutable_challenge;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class BankAccount {
     public enum AccountType {
         CHECKING, SAVINGS
     }
 
     private final AccountType accountType;
-    private final double balance;
+    private double balance;
+    private final Map<Long, Transaction> transactions = new LinkedHashMap<>();
 
     public BankAccount(AccountType accountType, double balance) {
         this.accountType = accountType;
@@ -21,8 +25,19 @@ public class BankAccount {
         return balance;
     }
 
+    public Map<Long, Transaction> getTransactions() {
+        return Map.copyOf(transactions);
+    }
+
     @Override
     public String toString() {
         return "%s $%.2f".formatted(accountType, balance);
+    }
+
+    void commitTransaction(int routingNumber, long transactionId,
+                           String customerId, double amount) {
+        balance += amount;
+        transactions.put(transactionId, new Transaction(routingNumber, transactionId,
+                Integer.parseInt(customerId), amount));
     }
 }
