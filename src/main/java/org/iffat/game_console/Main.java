943400e366d03cfdd78908d54967a55fb0d84268
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-31 16:00:29 +0700

    17.Challenge: Initializers and Constructors, A Pirate Game

--- src/main/java/org/iffat/game_console/Main.java
+++ src/main/java/org/iffat/game_console/Main.java
@@ -2,12 +2,23 @@ package org.iffat.game_console;
 
 import org.iffat.game_console.game.GameConsole;
 import org.iffat.game_console.game.ShooterGame;
+import org.iffat.game_console.pirate.Pirate;
+import org.iffat.game_console.pirate.Weapon;
 
 public class Main {
     public static void main(String[] args) {
-        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+//        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+//
+//        int playerIndex = console.addPlayer();
+//        console.playGame(playerIndex);
+        Weapon weapon = Weapon.getWeaponByChar('P');
+        System.out.println("weapon = " + weapon + ", hitPoints="
+                + weapon.getHitPoints() + ", minLevel=" +weapon.getMinLevel());
 
-        int playerIndex = console.addPlayer();
-        console.playGame(playerIndex);
+        var list = Weapon.getWeaponsByLevel(1);
+        list.forEach(System.out::println);
+
+        Pirate tim = new Pirate("Tim");
+        System.out.println(tim);
     }
 }
